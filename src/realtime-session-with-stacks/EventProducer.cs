f88435a71f1a7df2e2d30ba16d15d773f76f9819
Brian Robbins <brianrob@microsoft.com>
2022-09-15 15:31:33 -0700

    Realtime session with stacks and symbol resolution example

---
+++ src/realtime-session-with-stacks/EventProducer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace realtime_session_with_stacks
+{
+    [EventSource(Name = "EventProducer")]
+    internal class EventProducer : EventSource
+    {
+        internal static EventProducer Log = new EventProducer();
+
+        [Event(1)]
+        public void TestEvent()
+        {
+            WriteEvent(1);
+        }
+
+        internal static void LogEvents()
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while(true)
+                {
+                    Log.TestEvent();
+                    Thread.Sleep(1000);
+                }
+            },
+            TaskCreationOptions.LongRunning);
+        }
+    }
+}
