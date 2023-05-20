ac1e5fe1901a85f775e97315fcdf587bc82b2784
Brian Robbins <brianrob@microsoft.com>
2023-05-19 20:59:13 -0700

    Implementation of an EventListener that writes os_signpost events

---
+++ src/os-signposter-eventlistener/managed/signpost-forwarder/StartupHook.cs
@@ -0,0 +1,11 @@
+﻿using signpost_forwarder;
+
+internal class StartupHook
+{
+    private static Forwarder Instance;
+    public static void Initialize()
+    {
+        System.Console.WriteLine("Hello!");
+        Instance = new Forwarder();
+    }
+}
