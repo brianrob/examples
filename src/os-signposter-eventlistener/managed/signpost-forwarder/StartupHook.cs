0823db20d8a012ff5d258161601548c9a029753b
Brian Robbins <brianrob@microsoft.com>
2023-05-22 13:33:39 -0700

    Remove console write

--- src/os-signposter-eventlistener/managed/signpost-forwarder/StartupHook.cs
+++ src/os-signposter-eventlistener/managed/signpost-forwarder/StartupHook.cs
@@ -5,7 +5,6 @@ internal class StartupHook
     private static Forwarder Instance;
     public static void Initialize()
     {
-        System.Console.WriteLine("Hello!");
         Instance = new Forwarder();
     }
 }
